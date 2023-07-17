356448df5ac90b4a3bde9426c3818ab47392804f
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2023-07-17 23:53:36 +0200

    Understanding using the_title_attribute()

---
+++ .history/index_20230717234659.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<!-- Check If posts exist, if yes then execute while loop -->
+ 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <!-- Display Posts code here -->
+
+           <h2><?php the_title() ;?></h2>
+
+           <!-- Display the post thumbnail AKA featured image -->
+            <?php the_post_thumbnail(); ?> 
+
+            <!-- Display excerpt of the post -->
+            <?php the_excerpt(); ?> 
+
+<!-- End the while loop -->
+<?php endwhile; else : ?> 
+
+                       <p><?php _e( 'No Posts To Display.' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
---
+++ .history/index_20230717235159.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<!-- Check If posts exist, if yes then execute while loop -->
+ 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <!-- Display Posts code here -->
+
+           <h2>
+            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"> <?php the_title(); ?> </a>
+        </h2>
+
+           <!-- Display the post thumbnail AKA featured image -->
+            <?php the_post_thumbnail(); ?> 
+
+            <!-- Display excerpt of the post -->
+            <?php the_excerpt(); ?> 
+
+<!-- End the while loop -->
+<?php endwhile; else : ?> 
+
+                       <p><?php _e( 'No Posts To Display.' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
